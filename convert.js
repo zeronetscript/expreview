42dbf2ce78b5854eab7ec5ce3101d2ab56c9bd46
nobody <nogroup@nogroup.com>
2016-04-25 13:18:50 +0800

    support tag

--- convert.js
+++ convert.js
@@ -1,92 +1,105 @@
 "use strict"
-var feed = require("feed-read");
 var fs = require("fs")
 var jsonfile = require("jsonfile")
+var FeedParser = require('feedparser');
+var request = require('request')
 
+var feedparser = new FeedParser();
 
 
 
 
 var old_data = jsonfile.readFileSync("data.json");
 
-var next_post_id = old_data.next_post_id;
 
 function alreadyHave(title){
 
-    for (var i in old_data.post){
-        if(old_data.post[i].title==title){
-            return true;
-        }
+  for (var i in old_data.post){
+    if(old_data.post[i].title==title){
+      return true;
     }
+  }
 
-    return false;
+  return false;
 
 }
 
 function toUTC(date_object){
 
-    return date_object.getTime()/1000 + 8*60*60;
+  return date_object.getTime()/1000 + 8*60*60;
 }
 
 
-feed("http://www.expreview.com/rss.php", function(err, articles) {
-
-    if (err) throw err;
-    // Each article has the following properties:
-    // 
-    //   * "title"     - The article title (String).
-    //   * "author"    - The author's name (String).
-    //   * "link"      - The original article link (String).
-    //   * "content"   - The HTML content of the article (String).
-    //   * "published" - The date that the article was published (Date).
-    //   * "feed"      - {name, source, link}
-    // 
-    //
-
-    var old_first = articles.reverse();
-
-    var changed = false;
+var req = request("http://www.expreview.com/rss.php");
 
 
-    //temp remove first one 
-    var siteNotice = old_data.post[0];
-    old_data.post.splice(0,1);
-    old_data.next_post_id --;
+var changed = false;
 
-    for(var i in old_first){
-        var article = articles[i];
+feedparser.on('readable',function(){
 
-        if(alreadyHave(article.title)){
-            continue;
-        }
+  var stream = this;
+  var article;
 
-        var changed = true;
-
-        var post = {
-            'post_id': old_data.next_post_id,
-            'title':article.title,
-            'date_published': toUTC(new Date(article.published)),
-            'body':article.content
-        };
-
-        console.log("add:"+article.title);
-
-        old_data.post.unshift(post);
-
-        old_data.next_post_id = old_data.next_post_id+1;
+  while(article=stream.read()){
+    if(alreadyHave(article.title)){
+      console.log("skip "+article.title);
+      continue;
     }
 
-    siteNotice.post_id=old_data.next_post_id;
-    siteNotice.date_published=(new Date).getTime()/1000;
-    old_data.post.unshift(siteNotice);
-    old_data.next_post_id++;
+    changed = true;
+    var post = {
+      'post_id': old_data.next_post_id,
+      'title':article.title,
+      'date_published': toUTC(new Date(article.pubdate)),
+      'body':"---\n"+article.description
+    };
 
-    if (changed) {
-        old_data.modified = (new Date).getTime()/1000;
-        jsonfile.writeFileSync('new_data.json',old_data,{spaces:2});
-        console.log("write new file to new_data.json");
-    }else{
-        console.log("unchanged");
+    console.log("add:"+article.title);
+
+    old_data.post.unshift(post);
+
+    if (!old_data.tag) {
+      old_data.tag=[]
     }
+
+    var categories=article.categories;
+    for(var i in categories){
+      old_data.tag.push({value:categories[i],post_id:post.post_id});
+    }
+
+    old_data.next_post_id = old_data.next_post_id+1;
+
+  }
+
+
+
 
 });
+
+feedparser.on('end',function(){
+
+  if (!changed) {
+    console.log("unchanged");
+    return;
+  }
+
+  for (var i in old_data.post){
+    if (old_data.post[i].post_id==183) {
+      old_data.post[i].date_published = new Date().getTime()/1000;
+    }
+  }
+
+  old_data.modified = (new Date).getTime()/1000;
+  jsonfile.writeFileSync('new_data.json',old_data,{spaces:2});
+  console.log("write new file to new_data.json");
+});
+
+
+req.on('response',function(res){
+	    if (res.statusCode!=200) {
+		    console.log(thisOne.name+" error done");
+		    return;
+	    }
+
+	    res.pipe(feedparser);
+    });
